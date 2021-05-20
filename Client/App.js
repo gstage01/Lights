fa9018a05df547b16d68e8cf7308a28bcc65ad78
gstage01 <gstage01@hotmail.com>
2021-05-19 21:49:53 -0500

    Adding Client

---
+++ Client/App.js
@@ -0,0 +1,135 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { Component } from 'react';
+import Config from "./config.js";
+
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+
+
+class App extends Component {
+  state = {
+    connectingText: "Connected",
+    currAnimation: "",
+  };
+
+  animationLibrary = {
+    0: "Off",
+    1: "Chase",
+    2: "Alternating RGB",
+    3: "Red Fade",
+    4: "Rainbow",
+    5: "Rainbow With Glitter",
+    6: "BPM"
+  }
+
+  // Send new animation data
+  // Sets state to reflect changes in connection/animation
+  sendSignal = async (code) => {
+    let found = 0;
+    try {
+
+      // Handle timeout with a 5 second timer
+      let abort = new AbortController();
+      setTimeout(() => {
+        if (found) return;
+        throw new Error("Timeout: Time Exceeded");
+      }, 5000);
+
+      this.setState({
+        currAnimation: this.currAnimation,
+        connectingText: "Connecting...",
+      })
+      let res = await fetch(
+        "http://" + Config.server_ip + ":" + Config.server_port + "/animate",
+        {
+          method: 'POST',
+          signal: abort.signal,
+          headers: {
+            'Content-Type': "text/plain",
+          },
+          body: code
+      });
+      if (res) found = 1;       // Triggers the timeout to return without error
+
+      let currAnim = await res.text() + "";       // Payload is returned as plain/text
+      this.setState({
+          connectingText: "Connected",
+          currAnimation: this.animationLibrary[currAnim],
+      });                            // data holds the current animation from the ESP
+
+
+    // Used to catch the timeout and other errors
+    } catch (err) {
+      this.setState({
+        connectingText: err,
+        currAnimation: this.state.currAnimation
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('0')}>
+          <Text>Off</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('1')}>
+          <Text>Chase</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('2')}>
+          <Text>Alternating RGB</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('3')}>
+          <Text>Random Fade</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('4')}>
+          <Text>Rainbow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('5')}>
+          <Text>Rainbow With Glitter</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => this.sendSignal('6')}>
+          <Text>BPM</Text>
+        </TouchableOpacity>
+        <View>
+          <Text>Current Animation: { this.state.currAnimation }</Text>
+        </View>
+        <View>
+          <Text>Connection Status: { this.state.connectingText }</Text>
+        </View>
+      </View>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFF0",
+    //justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    width: 250,
+    height: 50,
+    backgroundColor: '#8BF9EF',
+    justifyContent:'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+})
+
+export default App;
